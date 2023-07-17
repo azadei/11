3efdc45e8f56deaf7c959093522c08b331106d0c
azadei <azadei@code.edu.az>
2023-07-17 14:58:41 +0400

    1

---
+++ 11/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        List<int> numbers = new List<int> { 5, 7, 13, 24, 6, 9, 8, 7 };
+
+       
+        Console.Write("How many records you want to display? : ");
+        int topCount = int.Parse(Console.ReadLine());
+
+       
+        Console.WriteLine("The top " + topCount + " records from the list are:");
+        IEnumerable<int> topRecords = numbers.OrderByDescending(num => num).Take(topCount);
+        foreach (int record in topRecords)
+        {
+            Console.WriteLine(record);
+        }
+
+    }
+}
